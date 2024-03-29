88455e47a779a2c58c8df806f7fcf28cd3cd2870
Thapelo Motubatse <Thapelo.Motubatse@liberty.co.za>
2024-03-29 16:45:01 +0200

    commit

--- Runninghill.Sentence.Assessment.Domain/Mappings/MappingExtensions.cs
+++ Runninghill.Sentence.Assessment.Domain/Mappings/MappingExtensions.cs
@@ -4,6 +4,8 @@ using Runninghill.Sentence.Assessment.Domain.Models;
 namespace Runninghill.Sentence.Assessment.Application.Common.Mappings;
 public static class MappingExtensions
 {
-    public static Task<PaginatedList<TDestination>> PaginatedListAsync<TDestination>(this IQueryable<TDestination> queryable, int pageNumber, int pageSize) where TDestination : class
-        => PaginatedList<TDestination>.CreateAsync(queryable.AsNoTracking(), pageNumber, pageSize);
+    public static Task<PaginatedList<T>> PaginatedListAsync<T>(this IQueryable<T> queryable, int pageNumber, int pageSize) where T : class
+    {
+       return PaginatedList<T>.CreateAsync(queryable.AsNoTracking(), pageNumber, pageSize);
+    }
 }
